d1776ea8b1d682f933dc08a6d3324e82cf40a383
fakeshadow <24548779@qq.com>
2020-07-11 08:46:49 +0800

    experiment lock free

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "no-send")]
 pub(crate) mod cell_pool;
 pub(crate) mod linked_list;
+pub(crate) mod lock_free_pool;
 #[cfg(not(feature = "no-send"))]
 pub(crate) mod spin_pool;
 pub(crate) mod timeout;
